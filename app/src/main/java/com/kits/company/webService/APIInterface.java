258303f6283a1f092aceb63d9dfce0be8079be89
milad <milad.mfz73@gmail.com>
2022-04-07 17:16:11 +0430

    1401_01_18

--- app/src/main/java/com/kits/company/webService/APIInterface.java
+++ app/src/main/java/com/kits/company/webService/APIInterface.java
@@ -160,6 +160,17 @@ public interface APIInterface {
                               @Field("Code") String Code,
                               @Field("MobileNumber") String MobileNumber);
 
+    @POST("index.php")
+    @FormUrlEncoded
+    Call<RetrofitResponse> Errorlog(
+            @Field("tag")         String tag
+            , @Field("ErrorLog")    String ErrorLog
+            , @Field("Broker")      String Broker
+            , @Field("DeviceId")    String DeviceId
+            , @Field("ServerName")  String ServerName
+            , @Field("StrDate")     String StrDate
+            , @Field("VersionName") String VersionName
+    );
 
 
 
